8e3378a7987a196b7e0ff65b4cbf3c849a99d5fe
cho minseo <jms529411@gmail.com>
2023-09-04 18:01:41 +0900

    add file mod

--- edittest.py
+++ edittest.py
@@ -8,9 +8,15 @@ print(b, a)
 print("python", a, b)
 
 number_school = 2394584903
+print(number_school)
 str_name = "python"
+print(str_name)
 bool_view = False
+print(bool_view)
 flt_pi = 3.14
+print(flt_pi)
 char_element = 'a'
+print(char_element)
 chars = "hello world"
+print(chars)
 print("number : ",number_school,"name : ", str_name, "bool :", bool_view,"flt_pi : 0", flt_pi,"character : ",char_element,"characters : ", chars)
